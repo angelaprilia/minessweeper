5dfa1b738bc7987b55258842de28ae214591b8af
Angel Aprilia <apriliaAngel22@outlook.com>
2022-12-08 14:13:53 +0700

    test

--- minessweeper/Program.cs
+++ minessweeper/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+<<<<<<< HEAD
+Console.WriteLine("selamat siang!");
+=======
 Console.WriteLine("Hello, World!");
+Console.WriteLine("AP");
+Console.WriteLine("123");
+Console.WriteLine("hai angel");
+>>>>>>> 341ed66f7a90613c4b9eb438e9065af5b44d90b9
